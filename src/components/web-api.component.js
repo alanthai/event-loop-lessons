367a0583addefbad92d0db6e7f740a0d404b694f
Alan Thai <alan.thai@rangle.io>
2017-11-12 13:51:27 -0500

    Small tweaks

--- src/components/web-api.component.js
+++ src/components/web-api.component.js
@@ -10,7 +10,7 @@ const WebApiItem = ({ item }) => (
 
 export const WebApi = ({ webApiItems }) => (
   <div className="webapi b--webapi-color b--solid bw1">
-    <strong className="mb1 db bg-webapi-color tc pa1 f4">Web Api</strong>
+    <strong className="mb1 db bg-webapi-color tc pa1 f4">Web API</strong>
     <div className="flex flex-column ma1">
       {
         webApiItems.map((item) =>
